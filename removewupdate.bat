2d60d9137d30d567d79e7042d85cf778a39c1e77
Mario Luis Aguayo, Jr <mlaguayojr@users.noreply.github.com>
2015-12-24 08:20:42 -0500

    Update removewupdate.bat
    I completely forget how to pipeline xD.

--- removewupdate.bat
+++ removewupdate.bat
@@ -4,19 +4,17 @@
 echo off
 color 0f
 cls
-echo Scanning system for %1 patch
 echo Finding System root
 set t=%comspec%
-echo Getting patch listing
-%t:~0,20%\dism.exe /online /get-packages /format:table > "%cd%\patches.log"
-echo Scanning for patch %1
+echo Getting package name for patch %1
+%t:~0,20%\dism.exe /online /get-packages /format:table | findstr /i /c:"%1" > patches.log
 call "%cd%\find.exe" %1
 for /f "delims=" %%i in ('type "%cd%\status.log"') do set output=%%i
-if %output% neq "not found" (set pkg=%output% && goto remove) else (echo %output%)
+if %output% neq "not found" (set pkg=%output% && goto remove) else (echo patch was %output% && exit)
 
 :remove
 echo Found package name for %1: %pkg%
 echo Removing package
 %t:~0,20%\dism.exe /Online /Remove-Package /PackageName:%pkg% /quiet /norestart
-msg * "Please restart your computer!"
+msg * "Please restart your computer."
 exit
